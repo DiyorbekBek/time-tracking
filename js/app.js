cc30273addebf2d6c252de9c8804060ae99f23c0
Diyorbek Umaraliyev <140650344+DiyorbekBek@users.noreply.github.com>
2024-04-05 15:43:17 +0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,36 @@
+// // let arr = ["Diyor", "Shoxrux", "Akhror", "Asror"];
+
+// // let result = [];
+
+// // result = arr.filter((frend) => {
+// //   return frend === "Diyor";
+// // });
+
+// // console.log(result);
+
+// const sortFunction = (arr) => {
+//   let zeroCount = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === 0) {
+//       arr.splice(i, 1);
+//       zeroCount++;
+//       i--;
+//     }
+//   }
+
+//   for (let i = 0; i < zeroCount; i++) {
+//     arr.push(0);
+//   }
+//   return arr;
+// };
+
+// const arr = [5, 0, 8, 2, 4, 1];
+
+// const result = sortFunction(arr);
+
+// console.log(result);
+
+// const str = " Hello there thanks for trying my Kata";
+
+// const resoult = str.trim();
+// console.log(resoult);
